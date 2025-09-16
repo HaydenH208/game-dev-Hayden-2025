d85b07084ef3be9b26e65b17addbbd8dd1a22bf7
HaydenH208 <johncbtdaw@gmail.com>
2025-09-16 14:31:06 -0700

    Update and rename t1a02-video-flac-hayden.html to t1a02-video-flac-hayden.js

--- public/t1a02-video-flac-hayden.js
+++ public/t1a02-video-flac-hayden.js
@@ -1,24 +1,24 @@
-<h1 align=center> t1a02-video-flac-hayden.html </h1>
+<h1 align=center> t1a02-video-flac-hayden.js </h1>
 <input id="mynum01" tpye="number" value "09" placeholder="enter number">
 <button onclick="mySquareFunction()">Go</button>
 <br><br>
 <span id="mySpan01"></span>
 
 <script>
-  function mySquareFunction() {
-    let userNum = parseFloar(document.getElementById('myNum01').value);
+  mySquareFunction() {
+     userNum = parseFloar(document.getElementById('myNum01').value);
 
-    let myvideoflac = {
+     myvideoflac = {
       numbers: [ 3, 8, usernum]
     };
 
-    let output = "";
+     output = "";
 
-    for (let i = 0; i <myvideoflac.numbers.length; i++) {
-      let num = myvideoflac.numbers[i];
-      let square = num * num;
+     (let i = 0; i <myvideoflac.numbers.length; i++) {
+       num = myvideoflac.numbers[i];
+       square = num * num;
 
-      if (square % 2 === 0) {
+       (square % 2 === 0) {
         output =+ num + " squared + " +square + "(even) <br>";
       } else {
         output +[ num + " squared = " + square + " (odd) <br>";
