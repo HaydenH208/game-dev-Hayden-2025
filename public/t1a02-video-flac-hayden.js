3e96126abda6445c5824c918a3ec073657780105
HaydenH208 <johncbtdaw@gmail.com>
2025-11-05 12:53:25 -0800

    Update t1a02-video-flac-hayden.js

--- public/t1a02-video-flac-hayden.js
+++ public/t1a02-video-flac-hayden.js
@@ -1,32 +1,41 @@
-<h1 align=center> t1a02-video-flac-hayden.js </h1>
-<input id="mynum01" tpye="number" value "09" placeholder="enter number">
+<!DOCTYPE html>
+<html>
+<head>
+  <title>t1a02-video-flac-hayden.js</title>
+</head>
+<body>
+
+<h1 align="center">t1a02-video-flac-hayden.js</h1>
+
+<input id="mynum01" type="number" value="9" placeholder="enter number">
 <button onclick="mySquareFunction()">Go</button>
 <br><br>
 <span id="mySpan01"></span>
 
 <script>
-  mySquareFunction() {
-     userNum = parseFloar(document.getElementById('myNum01').value);
+function mySquareFunction() {
+  const userNum = parseFloat(document.getElementById('mynum01').value);
 
-     myvideoflac = {
-      numbers: [ 3, 8, usernum]
-    };
+  const myvideoflac = {
+    numbers: [3, 8, userNum]
+  };
 
-     output = "";
+  let output = "";
 
-     (let i = 0; i <myvideoflac.numbers.length; i++) {
-       num = myvideoflac.numbers[i];
-       square = num * num;
+  for (let i = 0; i < myvideoflac.numbers.length; i++) {
+    const num = myvideoflac.numbers[i];
+    const square = num * num;
 
-       (square % 2 === 0) {
-        output =+ num + " squared + " +square + "(even) <br>";
-      } else {
-        output +[ num + " squared = " + square + " (odd) <br>";
-      }
+    if (square % 2 === 0) {
+      output += num + " squared = " + square + " (even)<br>";
+    } else {
+      output += num + " squared = " + square + " (odd)<br>";
     }
-
-    document.getElementById('mySpan01').innerHTML = output;
   }
- </script>
 
+  document.getElementById('mySpan01').innerHTML = output;
+}
+</script>
 
+</body>
+</html>
